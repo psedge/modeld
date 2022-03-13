9b42cc34c19669e6461ebbefe65dd964ec3f8266
psedge <peter.sedgewick@tink.com>
2022-03-13 17:10:23 +0100

    Map those shapes

--- src/diagram.js
+++ src/diagram.js
@@ -10,6 +10,11 @@ export function determineCellTypeFromStyling(style) {
     if (style.indexOf("rounded=1") !== -1) return 'db'
     if (style.indexOf("cylinder") !== -1) return 'db'
     if (style.indexOf("database") !== -1) return 'db'
+    if (style.indexOf("aws3.rds") !== -1) return 'db'
+    if (style.indexOf("aws3d.rds") !== -1) return 'db'
+    if (style.indexOf("aws4.rds") !== -1) return 'db'
+    if (style.indexOf("aws3d.application_server") !== -1) return 'app'
+    if (style.indexOf("aws4.ec2") !== -1) return 'app'
 
     return 'app'
 }
